88b634215831cf1eb23d7fec9e7db41479815572
Adrien <adrienv.sui@gmail.com>
2023-01-05 21:00:28 -0300

    add user exceptions

---
+++ cash_balance/modules/user/ext.py
@@ -0,0 +1,14 @@
+class UserNotFound(Exception):
+    '''
+    User not found
+    '''
+
+class WrongPassword(Exception):
+    '''
+    Wrond password
+    '''
+
+class UserAlreadyExists(Exception):
+    '''
+    User already exists
+    '''
