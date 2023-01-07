47b262ffacb621b85132ec2ba48da37d3ca1f822
Adrien <adrienv.sui@gmail.com>
2023-01-07 18:35:32 -0300

    autohreturn token

--- cash_balance/libs/api/auth.py
+++ cash_balance/libs/api/auth.py
@@ -50,4 +50,4 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    return payload
+    return payload['sub']
