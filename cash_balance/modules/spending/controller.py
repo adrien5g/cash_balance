ae3f1674dddd335e7ce0ce9994cb4099390cfdeb
Adrien <adrienv.sui@gmail.com>
2023-01-07 18:39:11 -0300

    add spending controller

--- cash_balance/modules/spending/controller.py
+++ cash_balance/modules/spending/controller.py
@@ -1,3 +1,4 @@
+import pprint
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
 from .entities import (
@@ -7,7 +8,7 @@ from .entities import (
 from libs.api.auth import get_current_user
 from libs.database import get_session
 
-from .models import AllSpendings
+from .models import Spending, AllSpendings, NewSpending, NewTag, NewTagResponse
 from .repositories import SpendingRepository
 
 
@@ -21,6 +22,34 @@ class SpendingController:
         spending_repository: SpendingRepository = Depends(SpendingRepository),
         session: Session = Depends(get_session)
     ):
-        spendings = spending_repository.get_spendings(user_uuid['sub'], session)
-        print(spendings)
-        return spendings
+        spendings = spending_repository.get_spendings(user_uuid, session)
+        list_of_spends = []
+        for current_spending in spendings:
+            tags = [{'uuid': tag.tag_id} for tag in current_spending.tags]
+            spending = current_spending.dict()
+            spending['tags'] = tags
+            list_of_spends.append(spending)
+        return {
+            'spendings': list_of_spends
+        }
+
+
+    @router.post('/new', response_model=Spending)
+    def new_spending(
+        spending: NewSpending,
+        user_uuid = Depends(get_current_user),
+        spending_repository: SpendingRepository = Depends(SpendingRepository),
+        session: Session = Depends(get_session)
+    ):
+        new_spending = spending_repository.new_spending(spending, user_uuid, session)
+        return new_spending.dict()
+
+    @router.post('/tag/new', response_model=NewTagResponse)
+    def new_tag(
+        tag: NewTag,
+        user_uuid = Depends(get_current_user),
+        spending_repository: SpendingRepository = Depends(SpendingRepository),
+        session: Session = Depends(get_session)
+    ):
+        new_tag = spending_repository.new_spending_tag(tag, user_uuid, session)
+        return new_tag.dict()
