6978938280fd2fbfe13ef7c9a5f23a41a1e932df
Adrien <adrienv.sui@gmail.com>
2023-01-07 18:38:19 -0300

    Update spending models

--- cash_balance/modules/spending/models.py
+++ cash_balance/modules/spending/models.py
@@ -1,10 +1,35 @@
 from typing import List
 from pydantic import BaseModel
+from datetime import date
 
 class Spending(BaseModel):
     name: str
     value: str
-    tags: List[int]
+    uuid: str
+
+class AllTagsModel(BaseModel):
+    uuid: str
+
+class AllSpendingsModel(BaseModel):
+    name: str
+    note: str
+    value: str
+    uuid: str
+    tags: List[AllTagsModel]
 
 class AllSpendings(BaseModel):
-    List[Spending]
+    spendings: List[AllSpendingsModel]
+
+class NewSpending(BaseModel):
+    name: str
+    note: str
+    value: str
+    spending_date: date
+    tags: List[str]
+
+class NewTag(BaseModel):
+    name: str
+    description: str
+
+class NewTagResponse(NewTag):
+    uuid: str
